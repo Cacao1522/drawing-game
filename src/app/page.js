65edb39e71c9b83ae950bd7d95cdbe1151f59b40
Cacao1522 <kaorukato2003@gmail.com>
2023-12-28 01:59:44 +0900

    お絵描き機能試作

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { fabric } from "fabric";
+//import { Canvas } from "fabric/fabric-impl";
+
+export default function Home() {
+  return (
+    <p>
+      <Link href={"draw"}>スタート</Link>
+    </p>
+  );
+}
