25e9ab54ed372d6735c096c6d587d18b21aab194
gafu2317 <ryutaanubisu66@gmail.com>
2024-01-02 14:35:05 +0900

    answerpage追加

---
+++ src/app/answer/page.js
@@ -0,0 +1,10 @@
+"use client";
+import Link from "next/link";
+import React, { useRef, useEffect, useState } from "react";
+import styles from "./page.module.css";
+import { Stack, Button, Slider } from "@mui/material";
+
+export default function Page() {
+  const width = 800;
+  const height = 500;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { fabric } from "fabric";
 //import { Canvas } from "fabric/fabric-impl";
 
 export default function Home() {
   return (
-    <p>
-      <Link href={"draw"}>ゲームスタート</Link>
-    </p>
+      <p>
+        <Link href={"draw"}>ゲームスタート</Link>
+        <br />
+        <Link href={"answer"}>回答スタート</Link>
+      </p>
   );
 }
