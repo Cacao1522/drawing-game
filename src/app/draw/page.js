8386f993b7572b0f254796d4456c283137a265f9
gafu2317 <ryutaanubisu66@gmail.com>
2024-01-10 15:46:16 +0900

    skywayd導入

--- src/app/draw/page.js
+++ src/app/draw/page.js
@@ -1,190 +1,82 @@
 "use client";
 import Link from "next/link";
 import React, { useRef, useEffect, useState } from "react";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import { Stack, Button, Slider } from "@mui/material";
+import {
+  nowInSec,
+  SkyWayAuthToken,
+  SkyWayContext,
+  SkyWayRoom,
+  SkyWayStreamFactory,
+  uuidV4,
+} from "@skyway-sdk/room";
 
 export default function Page() {
+  const token = new SkyWayAuthToken({
+    jti: uuidV4(),
+    iat: nowInSec(),
+    exp: nowInSec() + 60 * 60 * 24,
+    scope: {
+      app: {
+        id: "11d4c5d5-c728-42c5-945b-39d06ac94469",
+        turn: true,
+        actions: ["read"],
+        channels: [
+          {
+            id: "*",
+            name: "*",
+            actions: ["write"],
+            members: [
+              {
+                id: "*",
+                name: "*",
+                actions: ["write"],
+                publication: {
+                  actions: ["write"],
+                },
+                subscription: {
+                  actions: ["write"],
+                },
+              },
+            ],
+            sfuBots: [
+              {
+                actions: ["write"],
+                forwardings: [
+                  {
+                    actions: ["write"],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    },
+  }).encode("DTXnoQc3irUJiGSOkIoLyt8NLC8uJyO1s5VGERllZZ8=");
+
+  (async () => {
+    // 1
+    const localVideo = document.getElementById("local-video");
+
+    const { audio, video } =
+      await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream(); // 2
+
+    video.attach(localVideo); // 3
+    await localVideo.play(); // 4
+    const buttonArea = document.getElementById("button-area");
+    const remoteMediaArea = document.getElementById("remote-media-area");
+    const roomNameInput = document.getElementById("room-name");
+    const myId = document.getElementById("my-id");
+    const joinButton = document.getElementById("join");
+  })(); // 1
+
   const width = 800;
   const height = 500;
   let canvasRef = useRef(null);
   let mouseX = null;
   let mouseY = null;
-
-  const ws = new WebSocket("ws://localhost:3001");
-  ws.onopen = () => {
-    console.log("Connected to the signaling server");
-    startPeerConnection();
-  };
-
-  // ICE server URLs
-  let peerConnectionConfig = {
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-  };
-
-  // Data channel オプション
-  let dataChannelOptions = {
-    ordered: false,
-  };
-
-  // Peer Connection
-  let peerConnection;
-
-  // Data Channel
-  let dataChannel;
-
-  // ページ読み込み時に呼び出す関数
-  window.onload = function () {};
-
-  // 新しい RTCPeerConnection を作成する
-  function createPeerConnection() {
-    const pc = new RTCPeerConnection(peerConnectionConfig);
-
-    // ICE candidate 取得時のイベントハンドラを登録
-    pc.onicecandidate = function (evt) {
-      if (evt.candidate) {
-        // 一部の ICE candidate を取得
-        // Trickle ICE では ICE candidate を相手に通知する
-        console.log(evt.candidate);
-      } else {
-        console.log("ICE candidate gathering completed.");
-        // 全ての ICE candidate の取得完了（空の ICE candidate イベント）
-        // Vanilla ICE では，全てのICE candidate を含んだ SDP を相手に通知する
-        // （SDP は pc.localDescription.sdp で取得できる）
-
-        //メッセージを送信
-        const message = pc.localDescription.sdp;
-        ws.send(message);
-      }
-    };
-
-    pc.ondatachannel = function (evt) {
-      console.log("Data channel created:", evt);
-      setupDataChannel(evt.channel);
-      dataChannel = evt.channel;
-    };
-
-    return pc;
-  }
-
-  // ピアの接続を開始する
-  function startPeerConnection() {
-    // 新しい RTCPeerConnection を作成する
-    peerConnection = createPeerConnection();
-
-    // Data channel を生成
-    dataChannel = peerConnection.createDataChannel(
-      "test-data-channel",
-      dataChannelOptions
-    );
-    setupDataChannel(dataChannel);
-
-    // Offer を生成する
-    peerConnection
-      .createOffer()
-      .then(function (sessionDescription) {
-        console.log("createOffer() succeeded.");
-        return peerConnection.setLocalDescription(sessionDescription);
-      })
-      .then(function () {
-        // setLocalDescription() が成功した場合
-        // Trickle ICE ではここで SDP を相手に通知する
-        // Vanilla ICE では ICE candidate が揃うのを待つ
-        console.log("setLocalDescription() succeeded.");
-      })
-      .catch(function (err) {
-        console.error("setLocalDescription() failed.", err);
-      });
-  }
-
-  // Data channel のイベントハンドラを定義する
-  function setupDataChannel(dc) {
-    dc.onerror = function (error) {
-      console.log("Data channel error:", error);
-    };
-    dc.onmessage = function (evt) {
-      console.log("Data channel message:", evt.data);
-      let msg = evt.data;
-      document.getElementById("history").value =
-        "other> " + msg + "\n" + document.getElementById("history").value;
-    };
-    dc.onopen = function (evt) {
-      console.log("Data channel opened:", evt);
-    };
-    dc.onclose = function () {
-      console.log("Data channel closed.");
-    };
-  }
-
-  // 相手の SDP 通知を受ける
-  function setRemoteSdp() {
-    let sdptext = document.getElementById("remoteSDP").value;
-
-    if (peerConnection) {
-      // Peer Connection が生成済みの場合，SDP を Answer と見なす
-      let answer = new RTCSessionDescription({
-        type: "answer",
-        sdp: sdptext,
-      });
-      peerConnection
-        .setRemoteDescription(answer)
-        .then(function () {
-          console.log("setRemoteDescription() succeeded.");
-        })
-        .catch(function (err) {
-          console.error("setRemoteDescription() failed.", err);
-        });
-    } else {
-      // Peer Connection が未生成の場合，SDP を Offer と見なす
-      let offer = new RTCSessionDescription({
-        type: "offer",
-        sdp: sdptext,
-      });
-      // Peer Connection を生成
-      peerConnection = createPeerConnection();
-      peerConnection
-        .setRemoteDescription(offer)
-        .then(function () {
-          console.log("setRemoteDescription() succeeded.");
-        })
-        .catch(function (err) {
-          console.error("setRemoteDescription() failed.", err);
-        });
-      // Answer を生成
-      peerConnection
-        .createAnswer()
-        .then(function (sessionDescription) {
-          console.log("createAnswer() succeeded.");
-          return peerConnection.setLocalDescription(sessionDescription);
-        })
-        .then(function () {
-          // setLocalDescription() が成功した場合
-          // Trickle ICE ではここで SDP を相手に通知する
-          // Vanilla ICE では ICE candidate が揃うのを待つ
-          console.log("setLocalDescription() succeeded.");
-        })
-        .catch(function (err) {
-          console.error("setLocalDescription() failed.", err);
-        });
-      document.getElementById("status").value = "answer created";
-    }
-  }
-
-  // チャットメッセージの送信
-  function sendMessage() {
-    if (!peerConnection || peerConnection.connectionState != "connected") {
-      alert("PeerConnection is not established.");
-      return false;
-    }
-    let msg = document.getElementById("message").value;
-    document.getElementById("message").value = "";
-
-    document.getElementById("history").value =
-      "me> " + msg + "\n" + document.getElementById("history").value;
-    dataChannel.send(msg);
-
-    return true;
-  }
 
   // canvas に描いたストロークの座標情報をすべて保存
   const [allStrokes, setAllStrokes] = useState({
@@ -404,14 +296,6 @@ export default function Page() {
         <Button variant="outlined" onClick={redo} className={styles.button}>
           進む
         </Button>
-      </div>
-      <div>
-        <textarea
-          id="history"
-          cols="80"
-          rows="10"
-          readOnly="readonly"
-        ></textarea>
       </div>
     </>
   );
