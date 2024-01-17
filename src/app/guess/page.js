89bc37b553b958db8d55e2a654705fe2cb6b4745
gafu2317 <ryutaanubisu66@gmail.com>
2024-01-17 14:50:50 +0900

    エラー解決

--- src/app/guess/page.js
+++ src/app/guess/page.js
@@ -54,28 +54,11 @@ export default function Page() {
         <div ref={movingTextRef} className={styles.movingText}>
           {movingText}
         </div>
-      </div> */}
-
-//         <label>
-//           入力してください :
-//           <input
-//             id="inputText"
-//             type="text"
-//             size="20"
-//             className={styles.inputText}
-//             value={inputText}
-//             onChange={(e) => {
-//               setInputText(e.target.value);
-//             }}
-//           />
-//         </label>
-//         <button onClick={answer}>解答する</button>
-//         <span className={styles.ok}>{ok}</span>
+        </div> */}
       </div>
       <div className={styles.blank} />
       {/* <div className={styles.background_lower}></div>
       <div className={styles.background_upper}></div> */}
     </div>
-
   );
 }
